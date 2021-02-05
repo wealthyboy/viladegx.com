91eed2f73bbf841f63921ca6861da6597fbb58b9
Atam Jacob <jacob.atam@gmail.com>
2021-02-04 21:47:05 -0800

    Updated shipping

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,62 @@
 @extends('admin.layouts.app')
 
 @section('content')
+<div class="row">
+   <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+         <div class="card-content">
+            <p class="category">Number of Products</p>
+            <h3 class="card-title">{{ 0 }}</h3>
+         </div>
+         <div class="card-footer text-right">
+				<div class="stats">
+					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
+				</div>
+			</div>
+      </div>
+   </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+         <div class="card-content">
+            <p class="category">Number of Customers</p>
+            <h3 class="card-title">{{ 0 }}</h3>
+         </div>
+         <div class="card-footer text-right">
+				<div class="stats">
+					<i class="material-icons text-danger">forward</i> <a href="{{ route('customers.index') }}">View</a>
+				</div>
+			</div>
+      </div>
+    </div>
+   
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+         <div class="card-content">
+            <p class="category">Number of Today's Sales</p>
+            <h3 class="card-title">{{ 0 }}</h3>
+         </div>
+         <div class="card-footer text-right">
+				<div class="stats">
+					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
+				</div>
+			</div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <div class="card card-stats">
+         <div class="card-content">
+         <p class="category">Today's Sales Total </p>
+            <h3 class="card-title">{{ 0 }}</h3>
+         </div>
+         <div class="card-footer text-right">
+				<div class="stats">
+					<i class="material-icons text-danger">forward</i> <a href="#">View</a>
+				</div>
+			</div>
+      </div>
+    </div>
+  
+</div>
+
 
 @endsection
