0a62a9c82266a813aa66fef7e5b2b81ab45c2342
Atam Jacob <jacob.atam@gmail.com>
2021-02-09 13:46:26 -0800

    Added landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,10 +109,10 @@
                               <img class="img-fluid" src="https://avenuemontaigne.ng/uploads/7gk3dB6sc1T1joovkGcWZ5NzmUAOfOK4GjLLBBeF.jpg" />
                            </div> 
                            <div>
-                              <img class="img-fluid" src="https://avenuemontaigne.ng/uploads/7gk3dB6sc1T1joovkGcWZ5NzmUAOfOK4GjLLBBeF.jpg" />
+                              <img class="img-fluid" src="https://avenuemontaigne.ng/uploads/eR6I0yzb6DtHAyF7A6ZvqATwF9AMnvoAYIRdqPS0.jpg" />
                            </div> 
                            <div> 
-                              <img  class="img-fluid" src="https://avenuemontaigne.ng/uploads/7gk3dB6sc1T1joovkGcWZ5NzmUAOfOK4GjLLBBeF.jpg" />
+                              <img  class="img-fluid" src="https://avenuemontaigne.ng/uploads/IdysQzOEpBY3KfHYP9n1Gxn3ESZazA0SI9YfSxip.jpg" />
                            </div>
                         </div> 
                     </div>
