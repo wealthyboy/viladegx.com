2e0f67bc9868471318869688471c52e5e1c40bad
Atam Jacob <jacob.atam@gmail.com>
2021-02-18 08:12:02 -0800

    Added reservations

--- app/Http/Controllers/Admin/Reservation/ReservationController.php
+++ app/Http/Controllers/Admin/Reservation/ReservationController.php
@@ -2,31 +2,55 @@
 
 namespace App\Http\Controllers\Admin\Reservation;
 
+
+
+use App\Models\User;
+
+use App\Models\Image;
+use App\Models\Reservation;
+use App\Models\Activity;
+use App\Models\Http\Helper;
+use App\Models\SystemSetting;
+use Illuminate\Support\Str;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Redirect;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
+    protected $settings;
+
+    public function __construct()
+    {	  
+	  $this->settings =  SystemSetting::first();
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
+    {   
+        $reservations = Reservation::orderBy('created_at','desc')->paginate(30);
+        return view('admin.reservations.index',compact('reservations'));
     }
 
-    /**
+   /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        User::canTakeAction(2);
+        return view('admin.reservations.create',compact('product_attributes'));
     }
-
     /**
      * Store a newly created resource in storage.
      *
