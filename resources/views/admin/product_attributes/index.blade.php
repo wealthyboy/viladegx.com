f377d04352f51b46d6e7f4ea7dca4b95f9497198
Atam Jacob <jacob.atam@gmail.com>
2021-03-05 16:40:13 -0800

    updated fonts

--- resources/views/admin/product_attributes/index.blade.php
+++ resources/views/admin/product_attributes/index.blade.php
@@ -73,7 +73,7 @@
                             <select name="type" required="true" class="form-control">
                                 <option  value="" selected="">--Choose Type--</option>
                                 <option  value="reservation">Reservation</option>
-                                <option  value="art" >fashion</option>
+                                <option  value="fashion" >fashion</option>
                             </select>
                         </div>
 
