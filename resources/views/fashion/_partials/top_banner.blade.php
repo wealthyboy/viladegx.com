fefd304ad72c1877cae82cf2a89b924f2f774bd5
Atam Jacob <jacob.atam@gmail.com>
2021-03-10 11:34:10 -0800

    Updtaed cart

--- resources/views/fashion/_partials/top_banner.blade.php
+++ resources/views/fashion/_partials/top_banner.blade.php
@@ -1,13 +1,13 @@
-<div style="background-color: #c6302c;" class="top-notice text-white">
+
+<div class="top-notice text-white bg--gray mt-5">
     <div class="container-fluid text-center">
         <div class="row">
             
             <div class="col-12">
-                <h5 class="d-inline-block color--light text-uppercase  mb-0"><b><i class="fas fa-money-check"></i>
-                   GET 10% OFF THIS VALENTINE’S SEASON, USE CODE BEMYVAL AT CHECKOUT</b>
+                <h5 class="d-inline-block text-uppercase mb-0">
+                    Incoming | <a class="" href="#">Shop the latest arrivals here</a>
                 </h5>
             </div>
-            
 
         </div>
         
