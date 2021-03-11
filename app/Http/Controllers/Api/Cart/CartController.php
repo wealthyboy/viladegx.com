ea2cf9758e58d1aa13cab7ceb40e3a3614fe1186
Atam Jacob <jacob.atam@gmail.com>
2021-03-11 14:29:36 -0800

    Updtaed cart

--- app/Http/Controllers/Api/Cart/CartController.php
+++ app/Http/Controllers/Api/Cart/CartController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 use App\Models\ProductVariation;
+use App\Models\Product;
+
 use App\Models\Cart;
 use App\Models\User;
 use App\Models\SystemSetting;
