89037cadba14325c202db640887beaf898dc463a
Atam Jacob <jacob.atam@gmail.com>
2021-03-11 20:50:30 -0800

    Updated Controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/{page}/home';
+    public const HOME = '/fashion';
 
     /**
      * The controller namespace for the application.
