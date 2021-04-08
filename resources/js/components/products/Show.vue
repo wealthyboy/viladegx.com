7347cbdf39040c20a6c2ba2374f1212a12063276
Atam Jacob <jacob.atam@gmail.com>
2021-04-07 21:56:28 -0700

    Added name

--- resources/js/components/products/Show.vue
+++ resources/js/components/products/Show.vue
@@ -147,13 +147,13 @@
                                     <span>Wishlist</span> 
                                     <span  v-if="loading"  class="spinner-border spinner-border-sm float-right ml-3" role="status" aria-hidden="true"></span>
                                     <span class="float-right">
-                                        <svg class="e6iqrxh0 css-11mme3r-Icon">
-                                            <use xlink:href="#iconLoaded-starFill">
-                                               <symbol data-icon-id="starFill" data-icon-set="farfetch-2020" id="iconLoaded-starFill"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2l2.868 6.922L22 9.844l-5.11 4.804L18.225 22 12 18.322 5.776 22l1.333-7.352L2 9.844l7.132-.922z"></path></svg></symbol>
+                                        <svg class="wishlist-icon-fillled">
+                                            <use xlink:href="#iconStarFill">
+                                               <symbol data-icon-id="starFill" id="iconStarFill"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2l2.868 6.922L22 9.844l-5.11 4.804L18.225 22 12 18.322 5.776 22l1.333-7.352L2 9.844l7.132-.922z"></path></svg></symbol>
                                             </use>
                                         </svg>
-                                        <svg class="eh6lylo0 css-1dupl03-Icon"><use xlink:href="#iconLoaded-star">
-                                            <symbol data-icon-id="star" data-icon-set="farfetch-2020" id="iconLoaded-star"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2l2.868 6.922L22 9.844l-5.11 4.804L18.225 22 12 18.322 5.776 22l1.333-7.352L2 9.844l7.132-.922L12 2zm-1.49 8.816l-3.976.513 2.733 2.57-.745 4.11L12 15.955l3.478 2.056-.745-4.111 2.733-2.57-3.975-.514L12 7.219l-1.49 3.598z"></path></svg></symbol>
+                                        <svg class="wishlist-icon"><use xlink:href="#iconStar">
+                                            <symbol data-icon-id="star" data-icon-set="farfetch-2020" id="iconStar"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2l2.868 6.922L22 9.844l-5.11 4.804L18.225 22 12 18.322 5.776 22l1.333-7.352L2 9.844l7.132-.922L12 2zm-1.49 8.816l-3.976.513 2.733 2.57-.745 4.11L12 15.955l3.478 2.056-.745-4.111 2.733-2.57-3.975-.514L12 7.219l-1.49 3.598z"></path></svg></symbol>
                                             </use>
                                         </svg>
                                     </span>
@@ -218,6 +218,7 @@ import  LoginModal from '../auth/LoginModal.vue'
 import  RegisterModal from '../auth/RegisterModal.vue'
 import  { mapGetters,mapActions } from 'vuex'
 import  Pagination from '../pagination/Pagination.vue'
+import imageZoom from 'vue-image-zoomer';
 
 
 export default {
@@ -231,7 +232,8 @@ export default {
        Images,
        LoginModal,
        Pagination,
-       RegisterModal
+       RegisterModal,
+       imageZoom
     },
     data(){
         return {
