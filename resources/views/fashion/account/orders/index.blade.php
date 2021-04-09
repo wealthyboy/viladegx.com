b66818250d7d52a1a8205638a17720a18f1599e1
Atam Jacob <jacob.atam@gmail.com>
2021-04-09 14:05:17 -0700

    Added name

--- resources/views/fashion/account/orders/index.blade.php
+++ resources/views/fashion/account/orders/index.blade.php
@@ -7,39 +7,42 @@
                 <div class="col-md-3">
                     @include('fashion.account.nav')
                 </div>
-                <div class="col-md-9 bg--light p-5">
-                <h2 class="">Orders </h2>
+                <div class="col-md-9  p-5">
+                   <h2 class="">Orders </h2>
+                   <div class="card card-plain">
 
-                @if ($orders->count() )
-                    <div class="table-responsive">
-                        <table  id="" class="table table-shopping table-striped">
-                            <thead>
-                                <tr>
-                                    <td class="text-left bold text-uppercase">Order ID</td>
-                                    <td class="text-left bold">CUSTOMER</td>
-                                    <td class="text-left bold">DATE ADDED</td>
-                                    <td></td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($orders as $order)
-                                <tr>
-                                    <td class="text-left">#{{ $order->id }}</td>
-                                    <td class="text-left">{{ Auth::user()->fullname() }}</td>
-                                    <td class="text-left">{{ $order->created_at->format('d/m/y') }}</td>
-                                    <td class="text-right"><a href="/fashion/order/{{ $order->id}}"  class=" p-5"><i class="fa fa-eye"></i> view</a></td>
-                                </tr>
-                                @endforeach
-                                    
-                            </tbody>
-                        </table>
-                    </div> 
-                    @else
-                        <div class="text-center">
-                            <img  width="200" height="200" src="/images/utilities/empty_product.svg" /> 
-                            <p>No orders yet</p>
-                        </div>
-                    @endif    
+
+                        @if ($orders->count() )
+                            <div class="table-responsive">
+                                <table  id="" class="table table-shopping table-striped">
+                                    <thead>
+                                        <tr>
+                                            <td class="text-left bold text-uppercase">Order ID</td>
+                                            <td class="text-left bold">CUSTOMER</td>
+                                            <td class="text-left bold">DATE ADDED</td>
+                                            <td></td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($orders as $order)
+                                        <tr>
+                                            <td class="text-left">#{{ $order->id }}</td>
+                                            <td class="text-left">{{ Auth::user()->fullname() }}</td>
+                                            <td class="text-left">{{ $order->created_at->format('d/m/y') }}</td>
+                                            <td class="text-right"><a href="/fashion/order/{{ $order->id}}"  class=" p-5"><i class="fa fa-eye"></i> view</a></td>
+                                        </tr>
+                                        @endforeach
+                                            
+                                    </tbody>
+                                </table>
+                            </div> 
+                            @else
+                                <div class="text-center">
+                                    <img  width="200" height="200" src="/images/utilities/empty_product.svg" /> 
+                                    <p>No orders yet</p>
+                                </div>
+                            @endif  
+                    </div>  
                 </div>
             </div>
         </div>
