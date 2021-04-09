00da36bd147c2d50780c676c253347ffccc0e42a
Atam Jacob <jacob.atam@gmail.com>
2021-04-09 13:41:42 -0700

    Added name

--- resources/views/fashion/account/nav.blade.php
+++ resources/views/fashion/account/nav.blade.php
@@ -1,18 +1,30 @@
-<h2 class="page-title">Dashboard</h2>
 
-<div class="list-group">
-    <a href="/account" class="list-group-item list-group-item-action text-uppercase bold p-4"><i class="fa fa-user-circle"></i> Account</a>
-    <a href="/change/password" class="list-group-item list-group-item-action text-uppercase  bold p-4"><i class="fa fa-edit"></i> Change Password</a>
-    <a href="/orders"          class="list-group-item list-group-item-action text-uppercase bold p-4"><i class="fa fa-shopping-cart"></i> Orders</a>
-    <a href="/address"         class="list-group-item list-group-item-action  text-uppercase bold p-4"><i class="fa fa-map-marker"></i> Shipping Addresses</a>
-    <a href="#" 
-    onclick="event.preventDefault();
-            document.getElementById('logout-form').submit();"
-            class="list-group-item list-group-item-action bold text-uppercase p-4"><i class="fas fa-sign-out-alt left"></i> Logout
-            
-            
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>      
-    </a>
-</div>
+
+<div class="sidebar-overlay"></div>
+<div class="sidebar-toggle"><i class="fas fa-sliders-h"></i></div>
+<aside class="sidebar-shop col-lg-3 order-lg-first mobile-sidebar">
+   <div class="pin-wrapper" style="">
+      <div class="sidebar-wrapper" style="">
+        <h2 class="page-title">Dashboard</h2>
+
+        <div class="list-group">
+            <a href="/account" class="list-group-item list-group-item-action text-uppercase bold p-4"><i class="fa fa-user-circle"></i> Account</a>
+            <a href="/change/password" class="list-group-item list-group-item-action text-uppercase  bold p-4"><i class="fa fa-edit"></i> Change Password</a>
+            <a href="/orders"          class="list-group-item list-group-item-action text-uppercase bold p-4"><i class="fa fa-shopping-cart"></i> Orders</a>
+            <a href="/address"         class="list-group-item list-group-item-action  text-uppercase bold p-4"><i class="fa fa-map-marker"></i> Shipping Addresses</a>
+            <a href="#" 
+            onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();"
+                    class="list-group-item list-group-item-action bold text-uppercase p-4"><i class="fas fa-sign-out-alt left"></i> Logout
+                    
+                    
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>      
+            </a>
+        </div>
+        </div>
+   </div>
+</aside>
+<!-- End .col-lg-3 -->
+
