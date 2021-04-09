0c4f705bd7ee2efb165bff0b90d4b4e55b52c653
Atam Jacob <jacob.atam@gmail.com>
2021-04-09 04:38:54 -0700

    Added name

--- public/f/js/app.js
+++ public/f/js/app.js
@@ -6241,6 +6241,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
         }).done(function (res) {
           self.find('.product-wishlist-icon-fillled').removeClass('d-none');
           self.find('.product-wishlist-icon').addClass('d-none');
+          console.log(store);
           store.commit('appendToWishlist', res.data);
         })["catch"](function () {
           console.log(false);
