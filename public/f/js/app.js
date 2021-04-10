4a0886c2855262c34efe351fc24cd6b0cd1593b4
Atam Jacob <jacob.atam@gmail.com>
2021-04-10 10:12:42 -0700

    Added name

--- public/f/js/app.js
+++ public/f/js/app.js
@@ -4527,24 +4527,6 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
@@ -54737,7 +54719,7 @@ var render = function() {
       : _vm._e(),
     _vm._v(" "),
     _vm.$root.loggedIn
-      ? _c("div", { staticClass: "header-dropdown ml-4" }, [
+      ? _c("div", { staticClass: "ml-4" }, [
           _c(
             "a",
             {
@@ -54764,37 +54746,7 @@ var render = function() {
                 ]
               )
             ]
-          ),
-          _vm._v(" "),
-          _c("div", { staticClass: "header-menu " }, [
-            _c("ul", [
-              _vm._m(0),
-              _vm._v(" "),
-              _vm._m(1),
-              _vm._v(" "),
-              _c("li", [
-                _vm._m(2),
-                _vm._v(" "),
-                _c(
-                  "form",
-                  {
-                    staticStyle: { display: "none" },
-                    attrs: {
-                      id: "logout-form",
-                      action: "/fashion/logout",
-                      method: "POST"
-                    }
-                  },
-                  [
-                    _c("input", {
-                      attrs: { type: "hidden", name: "_token" },
-                      domProps: { value: _vm.$root.token }
-                    })
-                  ]
-                )
-              ])
-            ])
-          ])
+          )
         ])
       : _vm._e(),
     _vm._v(" "),
@@ -54878,57 +54830,7 @@ var render = function() {
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("li", [
-      _c(
-        "a",
-        { staticClass: "color--primary", attrs: { href: "/fashion/account" } },
-        [_c("i", { staticClass: "fas fa-user left mr-2" }), _vm._v("Account")]
-      )
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("li", [
-      _c(
-        "a",
-        { staticClass: "color--primary", attrs: { href: "/fashion/orders" } },
-        [
-          _c("i", { staticClass: "fas fa-sign-out-alt left mr-1" }),
-          _vm._v(" Orders")
-        ]
-      )
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c(
-      "a",
-      {
-        staticClass: "color--primary",
-        attrs: {
-          href: "/fashion/logout",
-          onclick:
-            "event.preventDefault();\n                                                    document.getElementById('logout-form').submit();"
-        }
-      },
-      [
-        _c("i", { staticClass: "fas fa-sign-out-alt left mr-1" }),
-        _vm._v(
-          "\n                                        \n                                        Logout\n                                    "
-        )
-      ]
-    )
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/components/navigation/NavIcon.vue
+++ resources/js/components/navigation/NavIcon.vue
@@ -7,29 +7,11 @@
         <a  v-if="!$root.loggedIn"  href="/fashion/login" class="header-icon  pl-1">         
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M14.85 14.282a6 6 0 10-5.699 0C5.962 14.867 3 16.23 3 17v4h18v-4c0-.77-2.962-2.132-6.15-2.718zM16 9a4 4 0 11-8 0 4 4 0 018 0zm3 8.748V19H5v-1.252c.377-.218.947-.48 1.673-.74C8.269 16.438 10.287 16 12 16s3.73.438 5.327 1.009c.726.259 1.296.521 1.673.74z"></path></svg>
         </a>
-        <div v-if="$root.loggedIn" class="header-dropdown ml-4">
+        <div v-if="$root.loggedIn" class="ml-4">
             <a href="/fashion/account" class="header-icon  pl-1">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M14.85 14.282a6 6 0 10-5.699 0C5.962 14.867 3 16.23 3 17v4h18v-4c0-.77-2.962-2.132-6.15-2.718zM16 9a4 4 0 11-8 0 4 4 0 018 0zm3 8.748V19H5v-1.252c.377-.218.947-.48 1.673-.74C8.269 16.438 10.287 16 12 16s3.73.438 5.327 1.009c.726.259 1.296.521 1.673.74z"></path></svg>
             </a>
 
-            <div class="header-menu ">
-                <ul>  
-                    <li><a class="color--primary" href="/fashion/account"><i class="fas fa-user left mr-2"></i>Account</a></li>
-                    <li><a class="color--primary" href="/fashion/orders"><i class="fas fa-sign-out-alt left mr-1"></i> Orders</a></li>
-                    <li>
-                        <a class="color--primary" href="/fashion/logout"
-                                                onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            <i class="fas fa-sign-out-alt left mr-1"></i>
-                                            
-                                            Logout
-                                        </a>
-                                        <form id="logout-form" action="/fashion/logout" method="POST" style="display: none;">
-                                            <input type="hidden" name="_token" :value="$root.token">
-                                        </form>
-                    </li>
-                </ul>
-            </div><!-- End .header-menu  -->
         </div>
             <a href="/fashion/wishlist" class="header-icon  pl-1 pr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2l2.868 6.922L22 9.844l-5.11 4.804L18.225 22 12 18.322 5.776 22l1.333-7.352L2 9.844l7.132-.922L12 2zm-1.49 8.816l-3.976.513 2.733 2.57-.745 4.11L12 15.955l3.478 2.056-.745-4.111 2.733-2.57-3.975-.514L12 7.219l-1.49 3.598z"></path></svg>
