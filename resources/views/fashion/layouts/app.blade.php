9d0ca740901d68bd9294a46381972d35f81812a1
Atam Jacob <jacob.atam@gmail.com>
2021-04-11 10:15:19 -0700

    Added na

--- resources/views/fashion/layouts/app.blade.php
+++ resources/views/fashion/layouts/app.blade.php
@@ -305,12 +305,7 @@
     @yield('page-scripts')
     <script type="text/javascript">
         @yield('inline-scripts')
-		$(".d-down.dont-show.Women.test").each(function(){
-			$(this).css("display", "block")
-			console.log(true)
-
-
-		})
+		
 		let nav = document.querySelectorAll('li.p-d-down')
 		let child_nav = document.querySelectorAll('li.d-down')
 		    nav.forEach(function(elm,key){
