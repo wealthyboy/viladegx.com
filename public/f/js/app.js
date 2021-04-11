93d9bf73ea19ad10e7224d2f42e175a7a5faa6fe
Atam Jacob <jacob.atam@gmail.com>
2021-04-11 11:01:52 -0700

    Added na

--- public/f/js/app.js
+++ public/f/js/app.js
@@ -6168,11 +6168,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
               o = e.outerWidth(),
               i = u(window).width() - 45 - t - o;
           console.log(e.parent().offset());
-          i < 0 ? e.css("left", "-" + t + "px") : e.css("left", "0px");
-
-          if (e.parent().hasClass('dont-show')) {
-            e.parent().addClass('test').css("height", "0px");
-          }
+          i < 0 ? e.css("left", "-" + t + "px") : e.css("left", "0px"); //if ( e.parent().hasClass('dont-show') ){ e.parent().addClass('test').css("height", "0px") }
         });
       }
 
